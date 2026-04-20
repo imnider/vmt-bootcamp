b19980df1bc79f932d005031184581b62b141d5f
Neider Vélez <velezzambranoneider@gmail.com>
2026-04-20 12:30:45 -0500

    feat: actualizar la base de datos

--- database/talent-insights/database.sql
+++ database/talent-insights/database.sql
@@ -459,21 +459,22 @@ WHERE
     OR (m.Code = 'posts'              AND p.Code IN ('POSTS/CREATE', 'POSTS/UPDATE', 'POSTS/DELETE'));
 GO
 
-CREATE TABLE EmailTemplates(
-	EmailTemplateId INT IDENTITY(1,1) NOT NULL,
+CREATE TABLE EmailTemplates (
+	EmailTemplateId INT IDENTITY(1, 1) NOT NULL,
 	Name VARCHAR(100) NOT NULL,
 	Subject VARCHAR(255) NOT NULL,
 	Body TEXT NOT NULL,
-	CreateAt DATETIME2 NOT NULL DEFAULT SYSUTCDATETIME()
+	CreatedAt DATETIME2 NOT NULL DEFAULT(SYSUTCDATETIME())
 );
 GO
 
-INSERT INTO EmailTemplates(Name, Subject, Body)
+INSERT INTO EmailTemplates (Name, Subject, Body)
 VALUES
-	('COLLABORATOR_REGISTER','Registro de usuario - Talent Insights', 'Se creó una cuenta con su correo electrónico. Su contraseña es: <strong>{{password}}</strong>'),
-	('AUTH_LOGIN_SUCCESS', 'Inicio de sesión exitoso - Talent Insights', 'Se inició sesión en su cuenta a las: <strong>{{datetime}}</strong>'),
-	('AUTH_LOGIN_FAILED', 'Inicio de sesión fallido - Talent Insights', 'Se intentó iniciar sesión en su cuenta, si no fue usted quien realizó esta acción, comuníquelo de inmediato a un administrador.')
-GO
+('COLLABORATOR_REGISTER',              'Registro de usuario - Talent Insights',            'Se creo una cuenta con su correo electrónico. Su contraseña es: <strong>{{password}}</strong>'),
+('AUTH_LOGIN_SUCCESS',                 'Inicio de sesión exitoso - Talent Insights',       'Se inicio sesión en su cuenta a las <strong>{{datetime}}</strong>'),
+('AUTH_LOGIN_FAILED',                  'Inicio de sesión fallido - Talent Insights',       'Se intentó iniciar sesión en su cuenta, si no fue usted quién realizó esta acción, comuniquelo de inmediato a un administrador'),
+('AUTH_REGISTER_EMAIL_VERIFICATION',   'Verificación de correo - Talent Insights',         'Hola, para continuar con su proceso de registro, necesita validar su correo electrónico, haciendo clic en el siguiente <a href="{{url}}">enlace</a>.'),
+('AUTH_RECOVER_PASSWORD_OTP',          'Recuperación de contraseña - Talent Insights',	   'Hola, el siguiente código le permitirá completar el proceso de cambio de contraseña para su cuenta: <strong>{{otp}}</strong>');
 
 -- ============================================================
 --  ÍNDICES para mejorar el rendimiento de consultas frecuentes
