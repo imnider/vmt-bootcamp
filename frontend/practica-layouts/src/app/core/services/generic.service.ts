b161fcc3381965b04eebbd37b43b876a516e0d68
Neider Vélez <velezzambranoneider@gmail.com>
2026-05-19 12:24:59 -0500

    feat: añadir servicio genérico

---
+++ frontend/practica-layouts/src/app/core/services/generic.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { Observable } from "rxjs";
+
+export abstract class GenericService<T> {
+    protected readonly http = inject(HttpClient);
+
+    constructor(protected apiURL: string) {}
+
+     getAll(): Observable<T[]> {
+        return this.http.get<T[]>(this.apiURL);
+    }
+
+    getById(id: string): Observable<T> {
+        return this.http.get<T>(`${this.apiURL}/${id}`);
+    }
+
+    create(payload: Partial<T>): Observable<T> {
+        return this.http.post<T>(this.apiURL, payload);
+    }
+
+    update(id: string, payload: Partial<T>): Observable<T> {
+        return this.http.put<T>(`${this.apiURL}/${id}`, payload);
+    }
+
+    delete(id: string): Observable<void> {
+        return this.http.delete<void>(`${this.apiURL}/${id}`);
+    }
+
+}
--- frontend/practica-layouts/src/app/features/services/product.service.ts
+++ frontend/practica-layouts/src/app/features/services/product.service.ts
@@ -1,19 +1,11 @@
-import { inject, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { environment } from "../../environment/environment";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
 import { IProduct } from "../interfaces/product.interface";
+import { GenericService } from "../../core/services/generic.service";
 
 @Injectable({providedIn: 'root'})
-export class ProductService {
-    private readonly http = inject(HttpClient)
-    private readonly apiURL = `${environment.apiURL}/products`;
-    
-    getAll(): Observable<IProduct[]> {
-        return this.http.get<IProduct[]>(`${this.apiURL}`);
-    }
-
-    getById(id: string): Observable<IProduct> {
-        return this.http.get<IProduct>(`${this.apiURL}/${id}`);
+export class ProductService extends GenericService<IProduct> {
+    constructor() {
+        super(`${environment.apiURL}/products`);
     }
 }
